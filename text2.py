881db247b44248fea8edbbf3fd58a0e9303c4741
orzz2 <62045367+orzz2@users.noreply.github.com>
2021-11-16 20:06:42 +0800

    Update text2.py

--- text2.py
+++ text2.py
@@ -9,7 +9,12 @@ chrome_options.add_argument("window-size=1000,800")
 chrome_options.add_argument("--no-sandbox")
 d = DesiredCapabilities.CHROME
 d['loggingPrefs'] = {'browser': 'ALL'}
-driver = webdriver.Chrome(chrome_options=chrome_options)
+# 设置中文
+options.add_argument('lang=zh_CN.UTF-8')
+# 更换头部
+options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"')
+
+driver = webdriver.Chrome(chrome_options=chrome_options) # 创建浏览器对象
 driver.get('http://www.baidu.com')
 print(driver.title)
 print('无头浏览器启动成功')
