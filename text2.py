f365f2b2acb20db215b7e86622725b5a9183a8a1
orzz2 <62045367+orzz2@users.noreply.github.com>
2021-11-16 21:43:00 +0800

    Update text2.py

--- text2.py
+++ text2.py
@@ -30,9 +30,10 @@ options.add_argument('user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like M
 # if user_agent:
 #   options.add_argument('user-agent=' + user_agent)
 
-# options = DesiredCapabilities.CHROME
-# options['loggingPrefs'] = {'performance': 'ALL'}
+d = DesiredCapabilities.CHROME
+d['loggingPrefs'] = {'performance': 'ALL'}
 browser = webdriver.Chrome(chrome_options=options)# 创建浏览器对象
+browser = webdriver.Chrome(desired_capabilities=d)
 browser.get('http://www.baidu.com')
 
 
